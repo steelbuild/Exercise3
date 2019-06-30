1d8e81b643f35060f13afab0e2bd22cf276a097b
kevbite <kev_bite@msn.com>
2019-06-30 15:22:58 +0100

    Build now uses API_Key.

--- build.ps1
+++ build.ps1
@@ -1,6 +1,8 @@
-for ($i = 1; $i -le 100; $i++ )
-{
-    Write-Progress -Activity "Building" -Status "$i% Complete:" -PercentComplete $i;
+
+Write-Host "Using API Key '$env:API_KEY' for current build"
+
+for ($i = 1; $i -le 100; $i++ ) {
+  Write-Progress -Activity "Building" -Status "$i% Complete:" -PercentComplete $i;
 }
 
 Write-Host "Build Complete" -ForegroundColor Green
